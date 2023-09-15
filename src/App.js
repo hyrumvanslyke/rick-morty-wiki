10d8b23635e2b18100d627a194bac6cce6b646d9
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2023-09-15 19:59:44 -0400

    Finished cards need to figure out formating for columns

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React, { useState, useEffect} from 'react'
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import Filters from "./components/Filters/Filters";
 import Cards from "./components/Cards/Cards";
+
 function App() {
+  let [pageNumber, setPageNumber] = useState(1)
+  let [fetchedData, setFetchedData] = useState([])
+  let {info, results} = fetchedData
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`
+
+  useEffect(() =>{
+      (async function(){
+          let data = await fetch(api)
+          .then(res=>res.json())
+          setFetchedData(data)
+      })()
+  }, [api])
+
   return (
     <div className="App">
       <h1 className="text-center helvetica my-4">
@@ -10,13 +27,11 @@ function App() {
       </h1>
 
       <div className="container">
-        <div className="row">
-          <div className="col-3">
+        <div className="row"> 
           <Filters />
-          </div>
-          <div className="col-8">
+          <div className="col-lg-8 col-12">
             <div className="row">
-              <Cards />
+              <Cards results={results}/>
             </div>
           </div>
         </div>
